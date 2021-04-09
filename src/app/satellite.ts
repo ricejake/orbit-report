73ec3a9f39f449b00d75e21e86e113e1ba6016b7
ricejake <ecirbocaj@yahoo.com>
2021-04-08 21:11:14 -0500

    Highlight Space Debris: Complete

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,11 +5,21 @@ export class Satellite {
     operational: boolean;
     launchDate: string;
 
+
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name = name;
         this.type = type;
         this.launchDate = launchDate;
         this.orbitType = orbitType;
         this.operational = operational;
+    }
+
+    shouldShowWarning () {
+        if(this.type.toLowerCase() === 'space debris'){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
